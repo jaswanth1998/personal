298c942812837f8482b0b304198308da07d4e5a3
“jaswanth1998” <jaswanthtata@gmail.com>
2024-08-31 12:57:18 -0400

    added aos animations

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,15 +1,15 @@
-import { FunctionComponent ,useEffect} from "react";
+import { FunctionComponent, useEffect } from "react";
 import ButtonPrimary from "../components/Button";
 import FrameComponent from "../components/FrameComponent";
 import styles from "./Landing.module.css";
-import brush from '../Assests/Skills/brush.svg';
-import cook from '../Assests/Skills/cook.svg';
-import event from '../Assests/Skills/event.svg';
-import laptop from '../Assests/Skills/laptop.svg';
-import pencil from '../Assests/Skills/pencil.svg';
-import magnifyingGlass from '../Assests/Skills/magnifying-glass.svg';
+import brush from "../Assests/Skills/brush.svg";
+import cook from "../Assests/Skills/cook.svg";
+import event from "../Assests/Skills/event.svg";
+import laptop from "../Assests/Skills/laptop.svg";
+import pencil from "../Assests/Skills/pencil.svg";
+import magnifyingGlass from "../Assests/Skills/magnifying-glass.svg";
 
-import profilePic from '../Assests/profilePic.png';
+import profilePic from "../Assests/profilePic.png";
 import Footer from "../components/Footer";
 import VerticalFormItemTextarea from "../components/VerticalFormItemTextarea";
 import ContactMe from "../components/ContactMe";
@@ -27,49 +27,47 @@ const Landing: FunctionComponent = () => {
   }, []);
   return (
     <>
-    <div className={styles.landing}>
-      <main className={styles.frameParent}>
-        <div className={styles.maskGroupParent}>
-          <img
-            className={styles.maskGroupIcon}
-            loading="lazy"
-            alt=""
-            src={profilePic}
-          />
-          <div className={styles.buttonParent}>
-            <ButtonPrimary title="Download CV" />
-            <ButtonPrimary />
+      <div className={styles.landing}>
+        <main className={styles.frameParent}>
+          <div className={styles.maskGroupParent} data-aos="zoom-in">
+            <img
+              className={styles.maskGroupIcon}
+              loading="lazy"
+              alt=""
+              src={profilePic}
+            />
+            <div className={styles.buttonParent}>
+              <ButtonPrimary title="Download CV" />
+              <ButtonPrimary />
+            </div>
           </div>
-        </div>
-        <div className={styles.hiIAmJaswanthViewMyProfiParent} data-aos="fade-right">
-          <h1 className={styles.hiIAmContainer}>
-            <p className={styles.hi}>Hi</p>
-            <p className={styles.hi}>I am Jaswanth</p>
-            <p className={styles.hi}>{`View my profile `}</p>
-          </h1>
-          <div className={styles.frameGroup}>
-            <div className={styles.instanceParent}>
-              <FrameComponent
-                cook="Developer"
-                iconParkOutlinecook={laptop}
-              />
-              <FrameComponent
-                cook="Designer"
-                iconParkOutlinecook={brush}
-              />
+          <div className={styles.hiIAmJaswanthViewMyProfiParent}>
+            <div data-aos="fade-right">
+              <h1 className={styles.hiIAmContainer}>
+                <p className={styles.hi}>Hi</p>
+                <p className={styles.hi}>I am Jaswanth</p>
+                <p className={styles.hi}>{`View my profile `}</p>
+              </h1>
             </div>
-            <div className={styles.instanceParent}>
-              <FrameComponent
-                cook="Content creator"
-                iconParkOutlinecook={pencil}
-              />
-              <FrameComponent
-              data-aos="fade-up"
-                cook="SEO Expert"
-                iconParkOutlinecook={magnifyingGlass}
-              />
-            </div>
-            {/* <div className={styles.instanceParent}>
+            <div className={styles.frameGroup}>
+              <div className={styles.instanceParent} data-aos="flip-left" >
+                <FrameComponent cook="Developer" iconParkOutlinecook={laptop} />
+                <FrameComponent cook="Designer" iconParkOutlinecook={brush}  />
+              </div>
+              <div className={styles.instanceParent} data-aos="flip-right">
+                <FrameComponent
+                
+                  cook="Content creator"
+                  iconParkOutlinecook={pencil}
+                />
+                <FrameComponent
+                
+                  cook="SEO Expert"
+                  
+                  iconParkOutlinecook={magnifyingGlass}
+                />
+              </div>
+              {/* <div className={styles.instanceParent}>
               <FrameComponent
                 cook="Event manager"
                 iconParkOutlinecook="/materialsymbolseventoutline.svg"
@@ -79,13 +77,12 @@ const Landing: FunctionComponent = () => {
                 iconParkOutlinecook="/iconparkoutlinecook.svg"
               />
             </div> */}
+            </div>
           </div>
-        </div>
-      </main>
-      
-    </div>
-    <ContactMe />
-    <Footer/>
+        </main>
+      </div>
+      <ContactMe />
+      <Footer />
     </>
   );
 };
